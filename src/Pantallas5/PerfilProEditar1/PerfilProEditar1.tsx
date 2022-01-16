f5f2bd867d7aeddb95ce568ed58b02cd63e54c0f
ecunto <ecunto@payvalida.com>
2022-01-15 22:58:29 -0300

    PerfilInfo creado

--- src/Pantallas5/PerfilProEditar1/PerfilProEditar1.tsx
+++ src/Pantallas5/PerfilProEditar1/PerfilProEditar1.tsx
@@ -4,11 +4,18 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { TextView, StyledText, StyledImgIcon, NotiView } from "./styles";
 import Colors from "../../../constants/Colors";
 import Portada from "./Portada/Portada";
+import PerfilInfo from "./PerfilInfo/PerfilInfo";
 
 export default function PerfilProEditar1() {
   return (
     <ScrollView>
       <Portada />
+      <PerfilInfo
+        nombre={"Alejandra Martinez"}
+        profesion={"Instructora de yoga"}
+        estrellas={"4.5"}
+        reviews={"128"}
+      />
     </ScrollView>
   );
 }
